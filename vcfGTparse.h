989cc804bb9ce8ffda4bc07b7f1f2a085505aa1e
kretzsch <wkretzsch@gmail.com>
2014-05-18 12:35:51 +0100

    Added empty header.

---
+++ vcfGTparse.h
@@ -0,0 +1,17 @@
+/* @(#)vcfGTparse.h
+ */
+
+#ifndef _VCFGTPARSE_HPP
+#define _VCFGTPARSE_HPP 1
+
+#include <boost/spirit>
+
+namespace vcfspirit{
+
+
+   void vcfGTparse
+}
+
+
+#endif /* _VCFGTPARSE_H */
+
